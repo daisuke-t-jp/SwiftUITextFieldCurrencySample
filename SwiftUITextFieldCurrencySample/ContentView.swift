3d48eec3c5624cb31107a75bfc283ea98402beee
daisuke-t-jp <daisuke.t.jp@gmail.com>
2019-11-19 23:39:42 +0900

    Add project

---
+++ SwiftUITextFieldCurrencySample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUITextFieldCurrencySample
+//
+//  Created by Daisuke TONOSAKI on 2019/11/19.
+//  Copyright © 2019 Daisuke TONOSAKI. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
